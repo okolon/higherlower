2816ae38963d54ef299cea55732c82ef1125afd8
Norman Okolo <50837940+okolon@users.noreply.github.com>
2023-02-14 17:09:27 +0000

    realised the code was half done so I just finished it

--- main.c
+++ main.c
@@ -13,6 +13,26 @@ int main()
   number = rand() % 10;
 
   printf("Welcome to Guess the Number!\n");
-  printf("I have thought of a number... Enter your guess\n");
-  scanf("%d\n", &guess);
+  printf("I have thought of a number between 1 and 10... Enter your guess\n");
+  
+
+  do 
+    {
+      scanf("%d", &guess);
+
+      if (guess < number)
+      {
+        printf("Nope, go higher\n");
+      }
+
+      else if (number > guess)
+      {
+        printf("Nope, go lower\n");
+      }
+
+      else 
+      
+        printf("Nice, you guessed the correct answer!");
+  
+    } while (guess != number);
 }
